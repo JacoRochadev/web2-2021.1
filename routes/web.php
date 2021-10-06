b284c2f718bc5f19b9d76f8a7292865f1aa13f8a
JacoRochadev <jacofernandes@outlook.com>
2021-10-06 02:08:34 -0300

    atividade1

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use app\Http\Controllers\ClenteController;
+use app\Http\Controllers\FornecedorController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +19,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/clientes',[ClenteController::class, 'show']);
+
+Route::get('/fornecedores', [FornecedorController::class, 'show']);
+
+
